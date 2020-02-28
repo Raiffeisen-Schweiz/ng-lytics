06e8cb8418581997120d1b35c9747beb4d9fb116
Steven Imhof <steven.imhof@raiffeisen.ch>
2020-02-28 15:40:55 +0100

    updated public api

--- projects/ng-lytics/src/public-api.ts
+++ projects/ng-lytics/src/public-api.ts
@@ -2,5 +2,8 @@
  * Public API Surface of ng-lytics
  */
 
+export * from './lib/config';
+export { NgLyticsAction, NgLyticsPageRequest } from './lib/models';
 export * from './lib/ng-lytics.module';
 export * from './lib/ng-lytics.service';
+export * from './lib/token';
